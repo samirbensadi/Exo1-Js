27b3f0f79dcd18ade2ac752952402ef87f61bdc8
samir bensadi <samirbensadi@gmail.com>
2016-09-27 16:23:18 +0200

    scriptfait

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-/* votre fonction ici*/
+var paragraphe = document.querySelector('p'); //js selectionne p et l'enregistre dans une variable paragraphe
+
+function changerStyle(){
+	paragraphe.style.color = 'white';
+	paragraphe.style.backgroundColor = 'black';
+	paragraphe.style.border = '10px dashed red';
+	paragraphe.style.padding = '5px';
+}
+
+
